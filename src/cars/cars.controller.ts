a6bde9a76511eed5a8abc5d2d3317a4d2011123b
adcc662 <adcc662@gmail.com>
2024-03-20 18:49:15 -0600

    Checking request

---
+++ src/cars/cars.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Param } from '@nestjs/common';
+
+@Controller('cars')
+export class CarsController {
+  private cars = ['Toyota', 'Ford', 'Chevy'];
+
+  @Get()
+  getAllCars() {
+    return this.cars;
+  }
+
+  @Get(':id')
+  getCarById(@Param('id') id: string) {
+    console.log({ id });
+    return this.cars[id];
+  }
+}
